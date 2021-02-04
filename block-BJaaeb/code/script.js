8f3d14f09f688ad48c4d2d14d7d31dbaaf238477
Manish Kushare <manishkushare9@gmail.com>
2021-02-04 22:07:57 +0530

    update index.js

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,6 +6,16 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
+function createInputElm(label,type = "text"){
+  let labelTag = document.createElement("label");
+  labelTag.innerText = `${label} :`;
+  console.log(labelTag);
+  let inputTag = document.createElement("input");
+  inputTag.type = type;
+  return labelTag.append(inputTag);
+
+  
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
@@ -22,6 +32,17 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+function createList(array){
+  let ul = document.createElement("ul");
+  // ul.innerHTML = " ";
+  array.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText = element;
+     ul.append(li);  
+  });
+  document.body.append(ul);
+  return;
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
@@ -40,7 +61,29 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 */
 
 // Your code goes here
+function createTodoList(array){
+  let ul = document.createElement("ul");
+  array.forEach(elem => {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    let input = document.createElement("input");
+    let span = document.createElement("span");
+    p.innerText = elem.name;
+    input.type = "checkbox";
+    if(elem.isDone){
+      input.checked = true;   
+      span.innerText = "C";
 
+    }
+    if(!elem.isDone){
+      input.checked = false;
+      span.innerText = "X";
+    }
+    li.append(p,input,span);
+    ul.append(li);
+  })
+  return document.body.append(ul);
+}
 // TEST
 createTodoList([
   { name: 'Learn DOM', isDone: false },
